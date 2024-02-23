9b1506d2dcea9159d910a91dd43fa03826729d58
Milena Bukal <milena.bukal@gmail.com>
2024-02-23 18:17:25 -0500

    Switch to Hugo

---
+++ old/scripts/preview.js
@@ -0,0 +1,9 @@
+// todo: read top x files from articles/ directory (ordered alphabetically, so the newest date is first
+// then call formatFile(filename) for each :) ?
+
+function formatFile(fileName) {
+	// todo
+	// use the title as link text, link to file
+	// use the custom date tag to display date
+	// include the first paragraph
+}
